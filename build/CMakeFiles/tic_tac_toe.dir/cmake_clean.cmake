cb9d8ad7f2cd88fe54782f586fd43a80c71da653
Lasse Wolpmann <lassewolpmann@outlook.de>
2025-07-11 11:06:27 +0300

    Added new build dir

---
+++ build/CMakeFiles/tic_tac_toe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tic_tac_toe.dir/main.c.o"
+  "CMakeFiles/tic_tac_toe.dir/main.c.o.d"
+  "tic_tac_toe"
+  "tic_tac_toe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tic_tac_toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
